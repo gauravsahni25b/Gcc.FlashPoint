340f90b939496bd84a50bcd7ee646bd15ce42fe6
gauravs <gauravs@blinds.com>
2018-01-09 14:58:23 -0600

    Business Logic for Multiple Gets and Post

--- Gcc.FlashPoint/Gcc.FlashPoint.Core/BusinessLogic/HttpCallTimeSpanCalculator.cs
+++ Gcc.FlashPoint/Gcc.FlashPoint.Core/BusinessLogic/HttpCallTimeSpanCalculator.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Formatting;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace Gcc.FlashPoint.Core.BusinessLogic
 {
@@ -14,18 +18,25 @@ namespace Gcc.FlashPoint.Core.BusinessLogic
             _client = new HttpClient();
         }
 
-        //public Task<TimeSpan> MeasureMultipleGetCalls()
-        //{
-            //Stopwatch watch = new Stopwatch();
-            //watch.Start();
-            //HttpResponseMessage response = await _client.GetAsync(path);
-            //watch.Stop();
-            //if (response.IsSuccessStatusCode)
-            //{
-            //    return watch.Elapsed;
-            //}
-            //throw new ApplicationException();
-       // }
+        public async Task<IEnumerable<TimeSpan>> MeasureNGetCalls(string path, int n)
+        {
+            List<TimeSpan> resultCollection = new List<TimeSpan>();
+            for (int i = 0; i < n; i++)
+            {
+                resultCollection.Add(await MeasureGetCall(path));
+            }
+            return resultCollection;
+        }
+
+        public async Task<IEnumerable<TimeSpan>> MeasureNPostCalls(string path, string[] jsonBodies)
+        {
+            List<TimeSpan> resultCollection = new List<TimeSpan>();
+            for (int i = 0; i < jsonBodies.Count(); i++)
+            {
+                resultCollection.Add(await MeasurePostCall(path, jsonBodies[i]));
+            }
+            return resultCollection;
+        }
 
         public async Task<TimeSpan> MeasureGetCall(string path)
         {
@@ -37,20 +48,20 @@ namespace Gcc.FlashPoint.Core.BusinessLogic
             {
                 return watch.Elapsed;
             }
-            throw new ApplicationException();
+            return new TimeSpan(0);
         }
 
         public async Task<TimeSpan> MeasurePostCall(string path, string jsonBody)
         {
             Stopwatch watch = new Stopwatch();
             watch.Start();
-            HttpResponseMessage response = await _client.PostAsync(path, new StringContent(jsonBody));
+            HttpResponseMessage response = await _client.PostAsJsonAsync(path, JObject.Parse(jsonBody));
             watch.Stop();
             if (response.IsSuccessStatusCode)
             {
                 return watch.Elapsed;
             }
-            throw new ApplicationException();
+            return new TimeSpan(0);
         }
     }
 }
