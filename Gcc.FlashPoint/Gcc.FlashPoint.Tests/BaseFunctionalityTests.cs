99223f3c08a865ce32b54212626e5081714a57d1
gauravs <gauravs@blinds.com>
2018-01-09 15:52:32 -0600

    Added Unit Tests for the Application So far

---
+++ Gcc.FlashPoint/Gcc.FlashPoint.Tests/BaseFunctionalityTests.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using FluentAssertions;
+using Gcc.FlashPoint.Core.BusinessLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gcc.FlashPoint.Tests
+{
+    [TestClass]
+    public class BaseFunctionalityTests
+    {
+        [TestMethod]
+        public void Get丨Request丨OnlyOnce()
+        {
+            HttpCallTimeSpanCalculator timeCalculator = new HttpCallTimeSpanCalculator();
+            var result = timeCalculator.MeasureGetCall("http://designbuilderapi.localtest.me/system/health-check").Result;
+            Debug.WriteLine($"Resultant Time : {result}");
+            result.Should().NotBe(new TimeSpan(0));
+        }
+
+        [TestMethod]
+        public void Get丨Request丨Multiple()
+        {
+        }
+
+        [TestMethod]
+        public void Post丨Request丨OnlyOnce()
+        {
+            HttpCallTimeSpanCalculator timeCalculator = new HttpCallTimeSpanCalculator();
+            string data = @"{
+                'customer': {
+                    'customerId': '8012045',
+                    'name': 'Roddie Brailsford',
+                    'email': 'Roddie@homedepot.com',
+                    'primaryPhone': '764-456-7890',
+                    'secondaryPhone': '9876543211',
+                    'address': '2525 OldFarm, Houston, TX 77079'
+                },
+                'store': {
+                    'associate': 'Vijay R',
+                    'storeAddress': '10255 Richmond Ave, Houston, TX, 77042',
+                            'storeNumber': '111',
+                            'storePhone': '8877995544'
+                    },
+                    'designs': [
+                    {
+                        'designGuid': '22D4A606-257D-413A-B812-45E5399FEFD4',
+                            'agreementNumber': 'CT-1234'
+
+                    }
+                    ]
+                }";
+            var result = timeCalculator.MeasurePostCall("http://designbuilderapi.localtest.me/window-treatments/sessions", data).Result;
+            Debug.WriteLine($"Resultant Time : {result}");
+            result.Should().NotBe(new TimeSpan(0));
+        }
+
+        [TestMethod]
+        public void Post丨Request丨Multiple()
+        {
+            HttpCallTimeSpanCalculator timeCalculator = new HttpCallTimeSpanCalculator();
+            string dataArray = @"[
+              {
+                'customer': {
+                    'customerId': '8012045',
+                    'name': 'Roddie Brailsford',
+                    'email': 'Roddie@homedepot.com',
+                    'primaryPhone': '764-456-7890',
+                    'secondaryPhone': '9876543211',
+                    'address': '2525 OldFarm, Houston, TX 77079'
+                },
+                'store': {
+                    'associate': 'Vijay R',
+                    'storeAddress': '10255 Richmond Ave, Houston, TX, 77042',
+                            'storeNumber': '111',
+                            'storePhone': '8877995544'
+                    },
+                    'designs': [
+                    {
+                        'designGuid': '22D4A606-257D-413A-B812-45E5399FEFD4',
+                            'agreementNumber': 'CT-1234'
+  
+                    }
+                    ]
+              },
+              {
+                'customer': {
+                    'customerId': '8012045',
+                    'name': 'Roddie Brailsford',
+                    'email': 'Roddie@homedepot.com',
+                    'primaryPhone': '764-456-7890',
+                    'secondaryPhone': '9876543211',
+                    'address': '2525 OldFarm, Houston, TX 77079'
+                },
+                'store': {
+                    'associate': 'Vijay R',
+                    'storeAddress': '10255 Richmond Ave, Houston, TX, 77042',
+                            'storeNumber': '111',
+                            'storePhone': '8877995544'
+                    },
+                    'designs': [
+                    {
+                        'designGuid': '22D4A606-257D-413A-B812-45E5399FEFD4',
+                            'agreementNumber': 'CT-1234'
+  
+                    }
+                    ]
+              }
+            ]";
+            var result = timeCalculator.MeasureNPostCalls("http://designbuilderapi.localtest.me/window-treatments/sessions", dataArray).Result;
+            result.ToList().ForEach(a =>
+            {
+                Debug.WriteLine($"Resultant Time : {a}");
+                a.Should().NotBe(new TimeSpan(0));
+            });
+        }
+    }
+}
---
+++ Gcc.FlashPoint/Gcc.FlashPoint.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Gcc.FlashPoint.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Gcc.FlashPoint.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1f092698-078d-4f11-8c99-aef86c8c4893")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
