ed404c0ccab01af828a1ac1c34c6c5458866cb3d
gauravs <gauravs@blinds.com>
2018-01-09 12:32:44 -0600

    Moved things around

--- Gcc.FlashPoint/Gcc.FlashPoint.Core/BusinessLogic/HttpCallTimeSpanCalculator.cs
+++ Gcc.FlashPoint/Gcc.FlashPoint.Core/BusinessLogic/HttpCallTimeSpanCalculator.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 
-namespace Gcc.FlashPoint.BusinessLogic
+namespace Gcc.FlashPoint.Core.BusinessLogic
 {
     public class HttpCallTimeSpanCalculator
     {
