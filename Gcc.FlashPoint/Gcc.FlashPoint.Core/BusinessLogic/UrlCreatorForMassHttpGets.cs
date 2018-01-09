33590e2879a9dec6a74a3948cd34ed6dc0b32346
gauravs <gauravs@blinds.com>
2018-01-09 17:11:54 -0600

    Added UrlCreateorFor Mass Http Gets

---
+++ Gcc.FlashPoint/Gcc.FlashPoint.Core/BusinessLogic/UrlCreatorForMassHttpGets.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Gcc.FlashPoint.Core.BusinessLogic
+{
+    public class UrlCreatorForMassHttpGets
+    {
+        public string Url { get; protected set; }
+
+        public UrlCreatorForMassHttpGets(string url, string jsonDataForUrlSegments, string jsonDataForQueryStrings)
+        {
+            Url = url;
+            if (!string.IsNullOrEmpty(jsonDataForUrlSegments))
+            {
+                ExtendUrl(jsonDataForUrlSegments, UrlParameterAppendOperationType.UrlSegment); 
+            }
+            if (!string.IsNullOrEmpty(jsonDataForQueryStrings))
+            {
+                ExtendUrl(jsonDataForQueryStrings, UrlParameterAppendOperationType.QueryString);
+            }
+        }
+
+        [SuppressMessage("ReSharper", "PossibleMultipleEnumeration")]
+        private void ExtendUrl(string dataForUrls, UrlParameterAppendOperationType operationType)
+        {
+            JObject dataObjct = JObject.Parse(dataForUrls);
+            var properties = dataObjct.Properties();
+            StringBuilder builder = new StringBuilder(Url);
+
+            if (operationType == UrlParameterAppendOperationType.UrlSegment)
+            {
+                foreach (var property in properties)
+                {
+                    builder.Append($"/{property.Name}/{property.Value.ToString()}");
+                }
+                Url = builder.ToString();
+            }
+            else
+            {
+                var propertiesArray = properties.ToArray();
+                for(int i=0; i< properties.Count(); i++)
+                {
+                    if (i == 0)
+                    {
+                        builder.Append($"?{propertiesArray[i].Name}={propertiesArray[i].Value.ToString()}");
+                    }
+                    else
+                    {
+                        builder.Append($"&{propertiesArray[i].Name}={propertiesArray[i].Value.ToString()}");
+                    }
+                }
+                Url = builder.ToString();
+            }
+        }
+
+        private enum UrlParameterAppendOperationType
+        {
+            UrlSegment,
+            QueryString
+        }
+    }
+}
---
+++ Gcc.FlashPoint/Gcc.FlashPoint.Tests/UrlCreatorForMassHttpGetsTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using FluentAssertions;
+using Gcc.FlashPoint.Core.BusinessLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gcc.FlashPoint.Tests
+{
+    [TestClass]
+    public class UrlCreatorForMassHttpGetsTests
+    {
+        [TestMethod]
+        public void JsonObjectProperties丨ConvertedToUrlSegments()
+        {
+            string dataForUrls = @"{
+              'Name': 'Zeus',
+              'Job': 'God',
+              'Title': 'ThunderGod',
+              'Address': 'MountOlympus'
+            }";
+            UrlCreatorForMassHttpGets urlCreator = new UrlCreatorForMassHttpGets("localhost:3000", dataForUrls, null);
+            urlCreator.Url.Should().Be("localhost:3000/Name/Zeus/Job/God/Title/ThunderGod/Address/MountOlympus");
+        }
+
+        [TestMethod]
+        public void JsonObjectProperties丨ConvertedToQueryStringParams()
+        {
+            string dataForQueryString = @"{
+              'Name': 'Zeus',
+              'Job': 'God',
+              'Title': 'ThunderGod',
+              'Address': 'MountOlympus'
+            }";
+            UrlCreatorForMassHttpGets urlCreator = new UrlCreatorForMassHttpGets("localhost:3000", null, dataForQueryString);
+            urlCreator.Url.Should().Be("localhost:3000?Name=Zeus&Job=God&Title=ThunderGod&Address=MountOlympus");
+        }
+
+        [TestMethod]
+        public void JsonObjectProperties丨ConvertedToUrlSegments丨ConvertedToQueryStringParams()
+        {
+            string data = @"{
+              'Name': 'Zeus',
+              'Job': 'God',
+              'Title': 'ThunderGod',
+              'Address': 'MountOlympus'
+            }";
+            UrlCreatorForMassHttpGets urlCreator = new UrlCreatorForMassHttpGets("localhost:3000", data, data);
+            urlCreator.Url.Should().Be("localhost:3000/Name/Zeus/Job/God/Title/ThunderGod/Address/MountOlympus?Name=Zeus&Job=God&Title=ThunderGod&Address=MountOlympus");
+        }
+    }
+}
